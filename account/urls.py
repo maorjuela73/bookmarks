3d8fbc3e195edf6aa876959b3a25825670b4be16
Miguel Orjuela <ma.orjuela73@gmail.com>
2020-11-17 22:24:08 -0500

    Formulario de cambio de contraseñá

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboad, name='dashboard'),
+
+    # rutas de cambio de contraseña
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
 ]
